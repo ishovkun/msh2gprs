52fea2bd8a01375d9ea08d81a65555dc3202c2c1
Igor Shovkun <igshov@gmail.com>
2018-09-21 17:45:10 -0700

    Squashme

--- src/angem/Plane.hpp
+++ src/angem/Plane.hpp
@@ -296,9 +296,6 @@ Plane<Scalar>::strike_angle() const
 
   Scalar rstrike_from_cos = acos(v1) - M_PI / 2.;
   Scalar rstrike_from_sin = asin(v2) - M_PI / 2.;
-  std::cout << "rstrike_from_sin = "<< rstrike_from_sin << std::endl;
-  std::cout << "rstrike_from_cos = "<< rstrike_from_cos << std::endl;
-
 
   Scalar strike;
   if (rstrike_from_sin >= 0 and rstrike_from_cos >= 0)
