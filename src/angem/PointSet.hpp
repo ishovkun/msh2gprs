af79451c64ba21ace44eddf6f1acad54b8a088c7
Igor Shovkun <ishovkun@stanford.edu>
2018-10-09 15:01:02 -0700

    Mege fastset branch

--- src/angem/PointSet.hpp
+++ src/angem/PointSet.hpp
@@ -55,19 +55,6 @@ class PointSet
 };
 
 
-// template<>
-// PointSet<3,double>::PointSet(const double tol)
-//     :
-//     tol(tol)
-// {
-//   __int128 nx = static_cast<__int128>
-//       (  std::cbrt( std::numeric_limits<__int128>::max() )  );
-
-//   upper = {tol*nx / 2, tol*nx / 2, tol*nx / 2};
-//   lower = -upper;
-// }
-
-
 template<int dim, typename Scalar>
 std::size_t PointSet<dim,Scalar>::find(const Point<dim,Scalar> &p)
 {
