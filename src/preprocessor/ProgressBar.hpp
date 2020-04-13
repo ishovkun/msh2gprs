5fc7d0d503a7beac9e989a3f1207b23033bb3d09
Igor Shovkun <igshov@gmail.com>
2020-04-13 09:05:42 -0700

    improve behavior of progress bar

--- src/preprocessor/ProgressBar.hpp
+++ src/preprocessor/ProgressBar.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <string>
 #include <iostream>    // provides cout
-// Unix only
+// Unix-only headers to get terminal width
 #include <sys/ioctl.h> //ioctl() and TIOCGWINSZ
 #include <unistd.h> // for STDOUT_FILENO
 
@@ -46,15 +46,24 @@ class ProgressBar
     size_t width = get_window_width_();
     if (_max_width != 0)
       width = std::min( _max_width, width );
-    std::cout << "\r" << _prefix << " [";
-    width -= _prefix.size() + 2;
-    const std::string suffix = "] " + std::to_string(_percentage) + " % ";
+
+    // write prefix
+    const std::string prefix = "\r"  + _prefix + " [";
+    std::cout << prefix;
+    width -= prefix.size();
+
+    // make and remember suffix
+    const std::string suffix = "] " + std::to_string(_percentage) + " %\r";
     width -= suffix.size();
-    const size_t nfull = size_t(double(_percentage) / 100.0 * width);
-    for (size_t i=0; i<nfull; ++i)
-      std::cout << "#";
-    for (size_t i=nfull; i<width; ++i)
+
+    // print bar
+    const size_t nfill = size_t(double(_percentage) / 100.0 * width);
+    for (size_t i=0; i<nfill; ++i)
+      std::cout << _fill;
+    for (size_t i=nfill; i<width; ++i)
       std::cout << ".";
+
+    // print suffix
     std::cout << suffix;
     std::cout << std::flush;
   }
@@ -71,6 +80,7 @@ class ProgressBar
   const size_t _nitems;
   size_t _max_width;
   size_t _percentage;
+  const std::string _fill = "█";
 };
 
 }  // end namespace util
