412814e05aa29a343142e06511d7eaab30a0c55f
Igor Shovkun <igshov@gmail.com>
2020-05-25 15:29:38 -0700

    hdf5 reader can read geomech data

--- postprocessor/GprsHDF5Reader.py
+++ postprocessor/GprsHDF5Reader.py
@@ -7,16 +7,27 @@ import os
 class GprsHDF5Reader:
     input_file = None
     n_blocks = 0
+    n_vert = 0
     current_step = 0
     data = pd.DataFrame()
+    gm_data = pd.DataFrame()
     times = None
 
     def __init__(self, file_name):
         self.input_file = h5py.File(file_name, "r")
         self.times = self.input_file["RESTART"]["time"][:]
         self.n_blocks = self.input_file["FLOW_CELL"]["pres"].shape[0]
+        if "GEOMECH" in self.input_file.keys():
+            self.n_vert = self.input_file["GEOMECH"]["disp_dims=2"].shape[0]
 
     def advanceTimeStep(self) -> bool:
+        if "GEOMECH" in self.input_file.keys():
+            gm_data = self.input_file["GEOMECH"]
+            gm_keys = gm_data.keys()
+            storage = np.zeros( [ self.n_vert, len(gm_keys) ] )
+            for i, key in enumerate(gm_keys):
+                storage[:, i] = gm_data[key][:, self.current_step]
+            self.gm_data = pd.DataFrame(storage, columns=gm_keys)
         data = self.input_file["FLOW_CELL"]
         keys = data.keys()
         storage = np.zeros( [ self.n_blocks, len(keys) ] )
@@ -34,6 +45,9 @@ class GprsHDF5Reader:
 
     def getData(self) -> pd.DataFrame:
         return self.data
+
+    def getMechData(self) -> pd.DataFrame:
+        return self.gm_data
 
     def getRelativePosition(self) -> float:
         return float(self.current_step) / float(len(self.times))
