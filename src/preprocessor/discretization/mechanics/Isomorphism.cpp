bd8ea0ca09565b212b57824c5310c3f42b556a74
Igor Shovkun <igshov@gmail.com>
2021-05-19 11:12:24 -0700

    cleanup isomorphism

--- src/preprocessor/discretization/mechanics/Isomorphism.cpp
+++ src/preprocessor/discretization/mechanics/Isomorphism.cpp
@@ -20,29 +20,12 @@ Isomorphism::Isomorphism(angem::Polyhedron<double> const &p1,
   algorithms::EdgePath path1(/*source */ 0, /*edge */ 0, g1, p1);
   if ( !path1.exist() ) throw std::runtime_error("Cannot build primary path on the reference element");
 
-  // std::cout << "vp1: ";
-  // for (auto v : path1.get_vertex_path())
-  //   std::cout << v << " ";
-  // std::cout << " ("<< path1.get_vertex_path().size() << ")" << std::endl;
-  // for (auto e : path1.get_local_edge_path())
-  //   std::cout << e << " ";
-  // std::cout << " (" << path1.get_local_edge_path().size() << ")" << std::endl;
-
   Graph g2 = build_vertex_graph_(p2);
   for (size_t start = 0; start < g2.nv(); ++start) {
     for (size_t ie = 0; ie < g2.degree(start); ++ie) {
       algorithms::EdgePathFollower path2(start, ie, g2, p2, path1);
 
-      // std::cout << "vp2: ";
-      // for (auto v : path2.get_vertex_path())
-      //   std::cout << v << " ";
-      // std::cout << " ("<< path2.get_vertex_path().size() << ")" << std::endl;
-      // for (auto e : path2.get_local_edge_path())
-      //   std::cout << e << " ";
-      // std::cout << " (" << path2.get_local_edge_path().size() << ")" << std::endl;
-
       if (path2.exist()) {
-        // build_ordering_(path1.get_vertex_path(), path2.get_vertex_path());
         _ordering = path2.get_vertex_mapping();
         return;
       }
