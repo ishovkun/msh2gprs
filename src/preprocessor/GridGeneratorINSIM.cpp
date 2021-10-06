38b880eceb7789a91a1ec0bf379a69ce64c702e4
Igor Shovkun <igshov@gmail.com>
2021-10-05 18:39:17 -0700

    made insim generator.cpp file

---
+++ src/preprocessor/GridGeneratorINSIM.cpp
@@ -0,0 +1,13 @@
+#include "GridGeneratorINSIM.hpp"
+
+namespace gprs_data {
+
+GridGeneratorINSIM::GridGeneratorINSIM()
+{}
+
+GridGeneratorINSIM::operator mesh::Mesh() const
+{
+  return mesh::Mesh();
+}
+
+}  // end namespace gprs_data
